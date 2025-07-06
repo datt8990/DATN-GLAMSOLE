eed07bfd85c1c2e0a2d984d24167b1ccf6076322
trinhkhanhduy2505 <trinhkhanhduy205@gmail.com>
2025-07-06 19:18:12 +0700

    fix: giao diện

--- FE/src/pages/admin/mausac/MauSacFilter.vue
+++ FE/src/pages/admin/mausac/MauSacFilter.vue
@@ -6,7 +6,14 @@
         <a-input id="search-query" v-model:value="localSearchQuery" placeholder="Nhập mã / tên để tìm kiếm..."
           class="search-input" />
       </div>
-
+      <div class="filter-item search-input-group">
+        <label for="search-query" class="filter-label">Trạng thái:</label>
+        <a-select class="select-input" v-model:value="localSearchStatus" allow-clear style="width: 180px" size="small"
+          placeholder="Chọn trạng thái">
+          <a-select-option :value="1">Hoạt động</a-select-option>
+          <a-select-option :value="0">Ngừng hoạt động</a-select-option>
+        </a-select>
+      </div>
       <div class="filter-item reset-button-group">
         <a-tooltip title="Làm mới bộ lọc">
           <a-button style="background-color: dimgrey; color: white;" @click="resetFilters" class="reset-button">
@@ -74,11 +81,13 @@ const resetFilters = () => {
 
 .filter-label {
   font-size: 14px;
-  font-weight: bold; /* This will now be effective */
+  font-weight: bold;
+  /* This will now be effective */
   margin-bottom: 5px;
   color: #555;
   white-space: nowrap;
 }
+
 
 .search-input {
   width: 700px;
