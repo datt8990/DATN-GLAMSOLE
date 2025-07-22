1ea9a7214e63c45ccff255e579f631a6b2e8d6e5
trinhkhanhduy2505 <trinhkhanhduy205@gmail.com>
2025-07-22 12:15:56 +0700

    feat: thêm bán hàng

--- BE/src/main/java/com/be/server/core/admin/hoadon/repository/ADHoaDonChiTietRepository.java
+++ BE/src/main/java/com/be/server/core/admin/hoadon/repository/ADHoaDonChiTietRepository.java
@@ -34,7 +34,6 @@ public interface ADHoaDonChiTietRepository extends HoaDonChiTietRepository {
                                                                         hd.phi_van_chuyen AS phiVanChuyen,
                                                                         pgg.ma_phieu_giam_gia AS maVoucher,
                                                                         hd.tong_tien_sau_giam AS tongTienSauGiam
-<<<<<<< HEAD
                                                                     FROM hoa_don_chi_tiet hdct
                                                                     LEFT JOIN hoa_don hd ON hdct.id_hoa_don = hd.id
                                                                     LEFT JOIN phieu_giam_gia pgg ON hd.id_voucher = pgg.id
@@ -45,18 +44,7 @@ public interface ADHoaDonChiTietRepository extends HoaDonChiTietRepository {
                                                                     LEFT JOIN kich_co ON spct.id_kich_co = kich_co.id
                                                                     LEFT JOIN mau_sac ON spct.id_mau_sac = mau_sac.id
                                                                     WHERE hd.ma_hoa_don = :maHoaDon
-=======
-                                                                    FROM db_datn.hoa_don_chi_tiet hdct
-                                                                    JOIN db_datn.hoa_don hd ON hdct.id_hoa_don = hd.id
-                                                                    JOIN db_datn.phieu_giam_gia pgg ON hd.id_voucher = pgg.id
-                                                                    JOIN db_datn.khach_hang kh ON hd.id_khach_hang = kh.id
-                                                                    JOIN db_datn.san_pham_chi_tiet spct ON hdct.id_spct = spct.id
-                                                                    JOIN db_datn.san_pham sp ON spct.id_san_pham = sp.id
-                                                                    JOIN db_datn.thuong_hieu ON sp.id_thuong_hieu = thuong_hieu.id
-                                                                    JOIN db_datn.kich_co ON spct.id_kich_co = kich_co.id
-                                                                    JOIN db_datn.mau_sac ON spct.id_mau_sac = mau_sac.id
-                                                                    WHERE db_datn.hd.ma_hoa_don = :maHoaDon
->>>>>>> ed50ec347cfaa1439b6aabeed1509324d3d24239
+
             """, nativeQuery = true)
     List<ADHoaDonChiTietResponseDetail> getAllHoaDonChiTietResponse(@Param("maHoaDon") String maHoaDon);
 }
